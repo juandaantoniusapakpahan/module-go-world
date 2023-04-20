a1b5b247e6d0fd475e9bff840aa419ba6ee78edc
juandaantoniusapakpahan <jujupapa06@gmail.com>
2023-04-20 14:10:03 +0700

    new world

--- world.go
+++ world.go
@@ -1,7 +1,7 @@
 package modulegoworld
 
-func World(name string) string {
-	return "Hello World"
+func World(name string, address string) string {
+	return "Hello World " + "I am " + name + ", I am from " + address
 }
 
 func Name(name string) string {
